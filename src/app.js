b0632b5ccf4d193d23a88ffbb993d2e883b6a1a2
Yarumis <yarumisginanrm@gmail.com>
2023-04-19 14:10:36 -0400

    domain iniciado

--- src/app.js
+++ src/app.js
@@ -8,4 +8,20 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let pronoun = ['the','our'];
+  let adj = ['great', 'big' ];
+  let noun = ['jogger','racoon'];
+
+  function domain (){
+     pronoun.forEach(elementPronoun => {
+      adj.forEach(elementAdj => {
+        noun.forEach(elementNoun => {
+           console.log(elementPronoun + elementAdj + elementNoun + ".com");
+  
+      });
+    });
+    
+  }); 
+}
 };
